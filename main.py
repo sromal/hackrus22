6d069c25a0f2fa440dcf46649475f1e8679cece8
Girish Ganesan <girishg0110@gmail.com>
2022-04-03 02:37:11 +0000

    dequeueing and interests matching fixed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from itertools import product
 import numpy as np
 
 pairings = {}
+interests = {}
 queue = []
 
 
@@ -33,7 +34,6 @@ def load_model():
 
 
 def cosine_similarity(word1, word2):
-    global model
     vec1 = model[word1]
     vec2 = model[word2]
     return vec1.dot(vec2) / (np.linalg.norm(vec1, 2) * np.linalg.norm(vec2, 2))
@@ -65,19 +65,24 @@ def send_message(message, recipient):
 # A route to respond to SMS messages
 @app.route('/sms', methods=['POST'])
 def incoming():
-    global pairings, queue
+    global pairings, queue, interests
     data = request.form['Body']
     sender_number = request.form['From']
 
     n_keywords = 3
     split = data.split(" ")
-    action = split[0]
-    keywords = split[1:n_keywords+1]
-
+    action = "" if len(split) == 0 else split[0]
+    
     if is_active(sender_number):
         if action == "!quit":
             partner = pairings.pop(sender_number)
             pairings.pop(partner)
+            interests.pop(sender_number)
+
+            send_message(
+                f'You finished your conversation!',
+                sender_number
+            )
 
             send_message(
                 f'{sender_number} disconnected! Reconnecting you to another match...',
@@ -88,43 +93,68 @@ def incoming():
         else:
             partner = pairings[sender_number]
             send_message(f'{data}', partner)
+    elif is_queued(sender_number):
+        if (action == "!quit"):
+            queue.remove(sender_number)
+            interests.pop(sender_number)
+
+            send_message(
+                f'You were dequeued!',
+                sender_number
+            )
     else:
         if action == "!start":
-            match(sender_number, keywords)
+            valid_terms = [term for term in split[1:] if term in model] if len(split) >= 2 else []
+            keywords = valid_terms[:min(n_keywords, len(valid_terms))]
+
+            interests[sender_number] = keywords
+            match(sender_number)
         else:
             send_message(f'Not connected! Please enter !start to get matched...', sender_number)
 
-    return None
+    return ""
 
 
-def match(sender_number, keywords):
+def match(sender_number):
     global pairings, queue
     if is_queued(sender_number) or is_active(sender_number):
         pass
     else:
-        most_compatible = (None, -np.inf)
-        for (possibleMatch, targetInterests) in queue:
+        most_compatible = (None, -np.inf, "", "")
+        chosenIndex = -1
+        keywords = interests[sender_number]
+        for i, possibleMatch in enumerate(queue):
+            targetInterests = interests[possibleMatch]
             for interest1, interest2 in product(keywords, targetInterests):
                 score = cosine_similarity(interest1, interest2)
                 if score > most_compatible[1]:
-                    most_compatible = (possibleMatch, score)
+                    most_compatible = (possibleMatch, score, interest1, interest2)
+                    chosenIndex = i
+            if (not targetInterests) and (not keywords):
+                most_compatible = (possibleMatch, 1.0, "anything", "anything")
+                chosenIndex = i
+        print(chosenIndex, most_compatible)
 
         threshold = 0.6
         if most_compatible[1] > threshold:
-            partner = most_compatible[0]
+            partner = queue.pop(chosenIndex)
+            pairings[sender_number] = partner
+            pairings[partner] = sender_number
+
+            topics = "anything" if not keywords else most_compatible[2] + " and " + most_compatible[3]
             send_message(
-                f"You've been matched with {partner}!",
+                f"You've been matched with {partner} to talk about {topics}!",
                 sender_number
             )
             send_message(
-                f"You've been matched with {sender_number}!",
+                f"You've been matched with {sender_number} to talk about {topics}!",
                 partner
             )
         else:
             queue.append(sender_number)
             send_message(f"Queued!", sender_number)
 
-    return None
+    return ""
 
 
 if __name__ == '__main__':
