b79ca64140043170b7f2bba39b00a77f868b7cd7
girishg0110 <girishg0110@gmail.com>
2022-04-02 20:17:11 -0400

    Added ML functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse, Message
 from twilio.rest import Client
 from dotenv import load_dotenv
+from itertools import product
+import numpy as np
 
 pairings = {}
 queue = []
@@ -13,7 +15,6 @@ def load_twilio_config():
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     number = os.environ['TWILIO_NUMBER']
-    print([account_sid, auth_token, number])
 
     if not all([account_sid, auth_token, number]):
         raise Exception
@@ -21,41 +22,64 @@ def load_twilio_config():
     return account_sid, auth_token, number
 
 
+def load_model():
+    model = {}
+    with open("glove.6B.50d.txt", "r", encoding="utf-8") as file:
+        for line in file.readlines():
+            parts = line.split(' ')
+            model[parts[0]] = np.array([float(x) for x in parts[1:]])
+
+    return model
+
+
+def cosine_similarity(word1, word2):
+    global model
+    vec1 = model[word1]
+    vec2 = model[word2]
+    return vec1.dot(vec2) / (np.linalg.norm(vec1, 2) * np.linalg.norm(vec2, 2))
+
+
 account_sid, auth_token, number = load_twilio_config()
 client = Client(account_sid, auth_token)
+model = load_model()
 app = Flask(__name__)
 
 
-def isQueued(sender_number):
+def is_queued(sender_number):
     return sender_number in queue
 
 
-def isActive(sender_number):
+def is_active(sender_number):
     return sender_number in pairings
 
 
-def sendMessage(message, recipient):
+def send_message(message, recipient):
     client.messages.create(
         body=message,
         from_=number,
         to=recipient
     )
+    return message
 
 
 # A route to respond to SMS messages
 @app.route('/sms', methods=['POST'])
 def incoming():
     global pairings, queue
-    print("Received!")
     data = request.form['Body']
     sender_number = request.form['From']
 
-    if isActive(sender_number):
-        if data == "!quit":
+    n_keywords = 3
+    split = data.split(" ")
+    action = split[0]
+    keywords = split[1:n_keywords+1]
+
+    if is_active(sender_number):
+        if action == "!quit":
             partner = pairings.pop(sender_number)
             pairings.pop(partner)
 
-            sendMessage(
+            send_message(
                 f'{sender_number} disconnected! Reconnecting you to another match...',
                 partner
             )
@@ -63,37 +87,44 @@ def incoming():
             return match(partner)
         else:
             partner = pairings[sender_number]
-            sendMessage(data, partner)
-            return data
+            send_message(f'{data}', partner)
     else:
-        if data == "!start":
-            return match(sender_number)
+        if action == "!start":
+            match(sender_number, keywords)
         else:
-            sendMessage(f'Not connected! Please enter !start to get matched...', sender_number)
-            return 'Not connected'
+            send_message(f'Not connected! Please enter !start to get matched...', sender_number)
 
-def match(sender_number):
+    return None
+
+
+def match(sender_number, keywords):
     global pairings, queue
-    if isQueued(sender_number) or isActive(sender_number):
-        return 'Already queued/active'
-    elif queue:
-        partner = queue.pop(0)
-        pairings[sender_number] = partner
-        pairings[partner] = sender_number
-
-        sendMessage(
-            f"You've been matched with {partner}!",
-            sender_number
-        )
-        sendMessage(
-            f"You've been matched with {sender_number}!",
-            partner
-        )
-        return partner
+    if is_queued(sender_number) or is_active(sender_number):
+        pass
     else:
-        queue.append(sender_number)
-        sendMessage("Queued!", sender_number)
-        return 'Queued'
+        most_compatible = (None, -np.inf)
+        for (possibleMatch, targetInterests) in queue:
+            for interest1, interest2 in product(keywords, targetInterests):
+                score = cosine_similarity(interest1, interest2)
+                if score > most_compatible[1]:
+                    most_compatible = (possibleMatch, score)
+
+        threshold = 0.6
+        if most_compatible[1] > threshold:
+            partner = most_compatible[0]
+            send_message(
+                f"You've been matched with {partner}!",
+                sender_number
+            )
+            send_message(
+                f"You've been matched with {sender_number}!",
+                partner
+            )
+        else:
+            queue.append(sender_number)
+            send_message(f"Queued!", sender_number)
+
+    return None
 
 
 if __name__ == '__main__':
